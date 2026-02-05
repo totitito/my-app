6d5a47de746b29a6b408d6ca8ae446147a96254f
totitito <shjoks@gmail.com>
2026-02-05 09:08:19 +0900

    save file명 원상복구

--- src/App.jsx
+++ src/App.jsx
@@ -201,7 +201,7 @@ function App() {
     const url = URL.createObjectURL(blob);
     const link = document.createElement("a");
     link.href = url;
-    link.download = `GHW_FULL_${ts}.json`;
+    link.download = `GHW_${ts}.json`;
     link.click();
   };
 
