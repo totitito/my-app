abc2e6f0e2c0b228c38ca0bf16370e88929484ab
totitito <shjoks@gmail.com>
2026-04-12 13:30:55 +0900

    e

--- src/App.jsx
+++ src/App.jsx
@@ -522,7 +522,7 @@ function App() {
           <div style={{ flexShrink: 0 }}>
             <h1 style={{ margin: "3px", marginLeft: "10px", fontSize: "56px", lineHeight: "0.9", fontWeight: "bold" }}>GHW</h1>
             <div style={{ fontSize: "11px", color: "#888", marginLeft: "10px", marginTop: "8px", whiteSpace: "nowrap" }}>
-              업데이트 : 2026-04-09 11:35
+              업데이트 : 2026-04-12 13:30
             </div>
           </div>
 
